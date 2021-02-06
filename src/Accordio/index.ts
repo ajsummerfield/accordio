d73e133ccb505e648b55c7404bac71d39735d681
Andy <aj.summerfield93@gmail.com>
2021-02-06 17:16:48 +0000

    Adding styles and exports

---
+++ src/Accordio/index.ts
@@ -0,0 +1,11 @@
+import Accordio from './Accordio';
+import AccordioItem from './AccordioItem';
+import AccordioHeading from './AccordioHeading';
+import AccordioContent from './AccordioContent';
+
+export {
+  Accordio,
+  AccordioItem,
+  AccordioHeading,
+  AccordioContent
+};
