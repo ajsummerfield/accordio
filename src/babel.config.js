0f72cdc6d12d222f058eeba8bab22a15f335533c
Andy <aj.summerfield93@gmail.com>
2021-03-15 22:45:12 +0000

    Updating package.json, babel and typescript for creating a build

--- src/babel.config.js
+++ src/babel.config.js
@@ -1,11 +1,19 @@
 module.exports = {
   presets: [
-    ['@babel/preset-env', {
-      targets: {
-        node: 'current'
+    ['@babel/preset-env'],
+    [
+      '@babel/preset-react',
+      {
+        'typescript': true
       }
-    }],
-    '@babel/preset-react',
-    '@babel/preset-typescript',
-  ],
+    ],
+    [
+      '@babel/preset-typescript',
+      {
+        'isTSX': true,
+        'allExtensions': true
+      }
+    ],
+    ['minify']
+  ]
 };
